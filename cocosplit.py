25b9bcffc8ca2392929213f16e05229990d7ec8e
cpwan <cpwan@ust.hk>
2021-09-24 13:31:13 +0800

    Update cocosplit.py
    minimal info is extracted, so that we can have a smaller size of the output json files

--- cocosplit.py
+++ cocosplit.py
@@ -15,10 +15,11 @@ parser.add_argument('--having-annotations', dest='having_annotations', action='s
 
 args = parser.parse_args()
 
-def save_coco(file, info, licenses, images, annotations, categories):
+def save_coco(file, images, annotations, categories):
     with open(file, 'wt', encoding='UTF-8') as coco:
-        json.dump({ 'info': info, 'licenses': licenses, 'images': images, 
-            'annotations': annotations, 'categories': categories}, coco, indent=2, sort_keys=True)
+        json.dump({ 'images': images, 
+            'annotations': annotations, 'categories': categories}, coco)
+    print(f"Saved {len(images)} entries in {file}.")
 
 def filter_annotations(annotations, images):
     image_ids = funcy.lmap(lambda i: int(i['id']), images)
@@ -27,25 +28,21 @@ def filter_annotations(annotations, images):
 def main(args):
     with open(args.annotations, 'rt', encoding='UTF-8') as annotations:
         coco = json.load(annotations)
-        info = coco['info']
-        licenses = coco['licenses']
         images = coco['images']
         annotations = coco['annotations']
         categories = coco['categories']
 
         number_of_images = len(images)
-
-        images_with_annotations = funcy.lmap(lambda a: int(a['image_id']), annotations)
-
+        
         if args.having_annotations:
+            images_with_annotations = funcy.lmap(lambda a: int(a['image_id']), annotations)
             images = funcy.lremove(lambda i: i['id'] not in images_with_annotations, images)
 
         x, y = train_test_split(images, train_size=args.split)
 
-        save_coco(args.train, info, licenses, x, filter_annotations(annotations, x), categories)
-        save_coco(args.test, info, licenses, y, filter_annotations(annotations, y), categories)
+        save_coco(args.train, x, filter_annotations(annotations, x), categories)
+        save_coco(args.test,  y, filter_annotations(annotations, y), categories)
 
-        print("Saved {} entries in {} and {} in {}".format(len(x), args.train, len(y), args.test))
 
 
 if __name__ == "__main__":
